32aac208b41bd423992ee55f5ba1099124605bad
senteou-ui <senteou@gmail.com>
2026-05-03 21:16:09 +0900

    hw3

--- lec03/homework3.py
+++ lec03/homework3.py
@@ -14,5 +14,16 @@ def words2characters(words):
     example, if words==['hello', 1.234, True], then characters should be
     ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
     """
-    return []
-
+def words2characters(words):
+    """
+    This function converts a list of words into a list of characters.
+    """
+    characters = []           
+    
+    for word in words:        
+        str_word = str(word)  
+        
+        for char in str_word:
+            characters.append(char) 
+            
+    return characters
